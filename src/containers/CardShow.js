ed7d9a6ee9ba6ce07b7c80aadaec7c91201ae3c7
tylertaylor <vikingtyty@gmail.com>
2018-01-27 10:08:01 -0600

    added removeFromCollection and toggleCardModal actions to CardShow component. added handleRemoveCard and handleShowModal functions

--- src/containers/CardShow.js
+++ src/containers/CardShow.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchCard, addToCollection } from '../actions/card-actions';
+import { fetchCard, addToCollection, removeFromCollection, toggleCardModal } from '../actions/card-actions';
 import { Card } from '../components/Card';
 
 class CardShow extends Component {
@@ -25,13 +25,36 @@ class CardShow extends Component {
         }
     }
 
+    handleRemoveCard = () => {
+        let id = this.props.match.params.id;
+        let token = this.props.token;
+
+        // user really shouldn't have the option to remove card without being logged in
+        // but just in case...
+        if (token) {
+            this.props.removeFromCollection(id, token);
+        } else {
+            this.props.history.push("/login")
+        }
+    }
+
+    handleShowModal = () => {
+        this.props.toggleCardModal(this.props.showCardModal)
+    }
+
     render() {
         const { card } = this.props;
 
         if (!card.errors) {
             return (
                 <div className="card-flex-container">
-                    <Card card={ card } inCollection={this.props.inCollection} add={this.handleAddCard}/>
+                    <Card card={ card } 
+                          inCollection={this.props.inCollection} 
+                          add={this.handleAddCard} 
+                          remove={this.handleRemoveCard}
+                          show={this.props.showCardModal} 
+                          showModal={this.handleShowModal}
+                    />
                 </div>
             )
         } else {
@@ -50,9 +73,10 @@ const mapStateToProps = (state) => {
     console.log(state)
     return ({
         card: state.card.card,
+        showCardModal: state.card.showCardModal,
         inCollection: state.card.inCollection,
         token: state.auth.token
     })
 }
 
-export default connect(mapStateToProps, { fetchCard, addToCollection })(CardShow);
+export default connect(mapStateToProps, { fetchCard, addToCollection, removeFromCollection, toggleCardModal })(CardShow);
